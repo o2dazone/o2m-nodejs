dd4143e0b3011e8512c77108baf07066431c422c
o2dazone <o2dazone@gmail.com>
2016-03-19 19:42:52 -0700

    cleaned up show results action

--- app/components/Container.js
+++ app/components/Container.js
@@ -3,21 +3,28 @@ import styles from 'styles/container.scss';
 import React from 'react';
 import { connect } from 'react-redux';
 import { isResultsVisible } from 'actions/results';
+import { toggleResults } from 'actions/results';
 import Sidebar from './Sidebar';
 import Results from './Results';
 
 export default class Container extends React.Component {
   constructor(props) {
     super(props);
+    this.onShowResults = this.onShowResults.bind(this);
+  }
+
+  onShowResults(e) {
+    e.preventDefault();
+    this.props.toggleResults(true);
   }
 
   render() {
     const { search, isResultsVisible } = this.props;
 
-    const results = search.query ? <Results /> : '';
+    const results = search.query ? <Results onShowResults={this.onShowResults} /> : '';
     return (
       <div className={styles.container}>
-        <Sidebar />
+        <Sidebar isResultsVisible={isResultsVisible} onShowResults={this.onShowResults} />
         {isResultsVisible ? results : ''}
         {/* {isResultsVisible ? results : <Playlists />}*/}
       </div>
@@ -33,4 +40,4 @@ function mapStateToProps(state) {
   };
 }
 
-export default connect(mapStateToProps, { isResultsVisible })(Container);
+export default connect(mapStateToProps, { isResultsVisible, toggleResults })(Container);
--- app/components/Sidebar.js
+++ app/components/Sidebar.js
@@ -3,24 +3,17 @@ import styles from 'styles/sidebar.scss';
 import React from 'react';
 import { connect } from 'react-redux';
 
-import { toggleResults } from 'actions/results';
-
 export default class Sidebar extends React.Component {
   constructor(props) {
     super(props);
   }
 
-  onShowResults(e) {
-    e.preventDefault();
-    this.props.toggleResults(true);
-  }
-
   render() {
-    const { isResultsVisible } = this.props;
+    const { isResultsVisible, onShowResults } = this.props;
 
     return (
       <div className={styles.sidebar}>
-        <span className={isResultsVisible ? styles.selected : ''} onClick={this.onShowResults}>Search Results</span>
+        <span className={isResultsVisible ? styles.selected : ''} onClick={onShowResults}>Search Results</span>
         <span>Playlists</span>
         <span className={styles.playlist}>Latest Additions</span>
         <span className={styles.playlist}>Start Listening</span>
@@ -29,11 +22,8 @@ export default class Sidebar extends React.Component {
   }
 }
 
-
-function mapStateToProps(state) {
-  return {
-    isResultsVisible: state.isResultsVisible
-  };
+function mapStateToProps() {
+  return { };
 }
 
-export default connect(mapStateToProps, { toggleResults })(Sidebar);
+export default connect(mapStateToProps, { })(Sidebar);
