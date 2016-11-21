0d04d18371ba256edab0b9291dc5b95a43f78539
o2dazone <o2dazone@gmail.com>
2016-11-21 13:55:12 -0800

    added happypack

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@
 var path = require('path');
 var webpack = require('webpack');
 var srcPath = path.join(__dirname, '/app');
-
+var HappyPack = require('happypack');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
@@ -31,15 +31,22 @@ module.exports = {
     new webpack.optimize.OccurenceOrderPlugin(),
     new webpack.HotModuleReplacementPlugin(),
     new webpack.NoErrorsPlugin(),
+    new HappyPack({
+      id: 'css-loaders',
+      loaders: [
+        'style-loader','css-loader?localIdentName=[local]___[hash:base64:5]',
+        'sass-loader?outputStyle=expanded'
+      ]
+    }),
     new webpack.DefinePlugin({
       'process.env.NODE_ENV': JSON.stringify('development')
     })
   ],
   module: {
     loaders: [
-      { test: /\.js?$/, exclude: /node_modules/, loader: 'babel', query: {"presets": ["react", "es2015", "stage-0", "react-hmre"]}},
+      { test: /\.js?$/, exclude: /node_modules/, loader: 'babel' },
       { test: /\.json?$/, loader: 'json'},
-      { test: /\.scss$/, loaders: ['style-loader','css-loader?localIdentName=[local]___[hash:base64:5]','sass-loader?outputStyle=expanded']},
+      { test: /\.scss$/, loaders: ['happypack/loader?id=css-loaders'] },
       { test: /\.(woff|woff2|ttf|eot|svg|gif|png|jpge?g)(\?v=\d+\.\d+\.\d+)?$/, loader: 'file-loader?name=[name].[hash].[ext]'}
     ]
   }
