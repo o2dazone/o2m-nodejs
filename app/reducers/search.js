9df3eec2675cce9d6d59ba6a02021010af864caf
o2dazone <o2dazone@gmail.com>
2017-02-09 09:16:04 -0800

    removed un-necessary reducer

--- app/reducers/search.js
+++ app/reducers/search.js
@@ -6,8 +6,6 @@ export default function search(state = { hasResults: false, results: [] }, actio
   switch (type) {
   case actionTypes.RECEIVE_SEARCH_RESULTS:
     return {...state, results, query, hasResults: results.length ? true : false};
-  case actionTypes.RECEIVE_APPENDED_SEARCH_RESULTS:
-    return {...state, results: [...state.results, ...results], query, hasResults: results.length ? true : false};
   default:
     return state;
   }
